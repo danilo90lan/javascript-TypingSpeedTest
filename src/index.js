bd307978c6933641acbcd874d175d2f33f642ed7
danilo90lan <15630@coderacademy.edu.au>
2024-11-09 10:21:18 +1100

    init typing speed  project

---
+++ src/index.js
@@ -0,0 +1,8 @@
+console.log("Typing Speed Test Begins!!");
+
+// Element Selectors
+const playButton = document.getElementById("playButton");
+const sentenceDisplay = document.getElementById("sentenceDisplay");
+const inputField = document.getElementById("inputField");
+const resultsSection = document.getElementById("resultsSection");
+
